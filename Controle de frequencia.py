ef03e12c11f437e1232bbdc635652ccaf4c231cb
Lanna Beatriz <delmondeslanna0@gmail.com>
2024-06-04 18:33:50 -0300

    Retirada de Codigos errados

--- Controle de frequencia.py
+++
@@ -1,8 +0,0 @@
-def controle_de_frequência(idade): 
-    if idade >= 18: 
-        print("Entrada Liberada.")
-    else: 
-        print("Não é permetido a entrada de menores de 18.")
-
-idade_visitante = int(input("Informe a sua idade: "))
-controle_de_frequência(idade_visitante)
