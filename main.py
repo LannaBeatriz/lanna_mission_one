df978b6a1882b34464aaf94569667e97434efca3
Lanna Beatriz <delmondeslanna0@gmail.com>
2024-06-05 17:08:32 -0300

    arquivo main recolocado

---
+++ main.py
@@ -0,0 +1,21 @@
+def sair():
+    print("Encerrando o programa...")
+
+def menu_principal():
+    while True:
+        print("\n*Calculadora de Conversão de Unidades*")
+        print("1. Conversor de Medidas")
+        print("2. Conversor de Temperaturas")
+        print("3. Sair")
+        opção = input("Digite a opção desejada: ")
+        if opção == "1":
+            from conversor_medidas import ConversorMedidas
+        elif opção == "2":
+            from conversor_temperaturas import Temperatura
+        elif opção == "3":
+            sair()
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+menu_principal()
